198ed7b5556d2e748425994c240e8d45f85b5ca7
micheal o dowd <micheal.odowd1989@gmail.com>
2022-04-27 15:32:44 +0100

    Adding Bank and Account classes

---
+++ WebApplication_Azure_Project/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication_Azure_Project
+{
+    public class Account
+    {
+        private string personName { set; get; }
+        private int accountNumber { set; get; }
+
+        private double balance = 0;
+
+        public Account(string personName, int accountNumber)
+        {
+            this.personName = personName;
+            this.accountNumber = accountNumber;
+        }
+
+        public void deposit(double amount)
+        {
+            this.balance += amount;
+        }
+
+        public double getBalance()
+        {
+            return balance;
+        }
+
+        public double getAccountNumber()
+        {
+            return accountNumber;
+
+        }
